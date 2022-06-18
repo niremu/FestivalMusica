4a708b7a24edcd81be809163a06f4c868741510f
Nico Reyes <reyes.nrm@gmail.com>
2022-06-18 15:44:16 -0400

    Compilamos un poco de SASS con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-function tarea(done) {
-    console.log('Mi primer tarea');
+const { src, dest } = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
 
-    done();
+function css (done) {
+    src('src/scss/app.scss')            // Identificar el archivo de SASS
+        .pipe( sass() )                 // Compilarlo
+        .pipe(dest("build/css"));       // Almacenarla en el disco duro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
-exports.primerTarea = tarea;
+exports.css = css ;
