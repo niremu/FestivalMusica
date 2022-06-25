375b0dfdb62b9cb23e59cd2cdea1d6a36aaa6d40
Nico Reyes <reyes.nrm@gmail.com>
2022-06-25 17:18:24 -0400

    finalizamos la galeria

--- build/js/app.js
+++ build/js/app.js
@@ -32,11 +32,30 @@ function mostrarImagen(id) {
     <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
     `;
 
-
+    // Crea el Overlay con la imagen
     const overlay = document.createElement('DIV');
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
 
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent='X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function(){
+
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+
+    // Añade el Overlay al HTML
     const body = document.querySelector('body');
     body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
